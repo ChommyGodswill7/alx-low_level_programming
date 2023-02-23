dc956a8bc36d22c2d886ef72b968c871bb76cbc2
ChommyGodswill7 <godswillchioma911@gmail.com>
2023-02-22 19:45:04 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - A main function
+ *
+ * print_alphabet - A function that prints alphabet in lowercase
+ *
+ *  Return: always 0
+ */
+void print_alphabet(void);
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar('i');
+		_putchar('\n');
+}
